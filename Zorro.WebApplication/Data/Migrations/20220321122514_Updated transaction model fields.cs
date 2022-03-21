02097973f668c84af96f70a4ef7e2a5f68b7da48
Luis D'abruzzo <s3630513@student.rmit.edu.au>
2022-03-21 23:25:37 +1100

    Updated transaction model

---
+++ Zorro.WebApplication/Data/Migrations/20220321122514_Updated transaction model fields.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zorro.WebApplication.Data.Migrations
+{
+    public partial class Updatedtransactionmodelfields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CompletedDateTime",
+                table: "Transactions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDateTime",
+                table: "Transactions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "TransactionStatus",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompletedDateTime",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDateTime",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionStatus",
+                table: "Transactions");
+        }
+    }
+}
--- Zorro.WebApplication/Models/Transaction.cs
+++ Zorro.WebApplication/Models/Transaction.cs
@@ -5,11 +5,22 @@
         public Guid ID { get; set; }
         public TransactionType TransactionType { get; set; }
         public decimal Amount { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public DateTime CompletedDateTime { get; set; }
+        public TransactionStatus TransactionStatus { get; set; } = TransactionStatus.Pending;
+
     }
 
     public enum TransactionType
     {
         Payment,
         Refund
+    }
+
+    public enum TransactionStatus
+    {
+        Approved,
+        Declined,
+        Pending
     }
 }

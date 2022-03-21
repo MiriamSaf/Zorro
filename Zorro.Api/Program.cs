7466b2989c9e9cc1aa853d5d2df154133bbfa46f
Luis D'abruzzo <s3630513@student.rmit.edu.au>
2022-03-21 23:44:27 +1100

    Fixed bug with signing in after identity changes. Added a minimal API for eCommerce integration.

---
+++ Zorro.Api/Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/ProcessPayment", async (TransactionRequest transactionRequest) =>
+{
+    Console.WriteLine($"Received eCommerce payment request of {transactionRequest.Amount:c} for UserID {transactionRequest.UserID}");
+
+    return new TransactionResponse();
+});
+
+app.Run();
+
+class TransactionRequest
+{
+    public TransactionRequest()
+    {
+    }
+
+    public Guid UserID { get; set; }
+    public decimal Amount { get; set; }
+}
+
+public class TransactionResponse
+{
+    public string ReceiptNumber => "123456";
+}

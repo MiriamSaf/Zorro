2676298b1b0165a66f594189f1556e5293177ee5
Luis D'abruzzo <s3630513@student.rmit.edu.au>
2022-03-28 22:07:20 +1100

    Updated transactions page to list sample data

---
+++ Zorro.WebApplication/Dtos/TransactionDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Zorro.WebApplication.Dtos
+{
+    public class TransactionDto
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; } = "";
+        public string RecipientId { get; set; } = "";
+        public decimal Amount { get; set; }
+    }
+}

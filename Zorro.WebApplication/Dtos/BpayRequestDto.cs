46eee78ed41a024a4f940ddcf1025a1126ce215a
Luis D'abruzzo <s3630513@student.rmit.edu.au>
2022-03-30 20:18:08 +1100

    Added a dto for Bpay requests and implemented it into the markup

---
+++ Zorro.WebApplication/Dtos/BpayRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Zorro.WebApplication.Dtos
+{
+    public class BpayRequestDto
+    {
+        public string BillerCode { get; set; } = "";
+        public bool RememberBiller { get; set; }
+        public string CustomerReferenceNumber { get; set; } = "";
+        public decimal Amount { get; set; }
+        public DateOnly Date { get; set; }
+    }
+}

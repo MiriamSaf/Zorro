fc3cede8b3a7c02f8302a0e9bbde856990df90f1
Damien MacDown <S3493516@student.rmit.edu.au>
2022-04-05 10:34:59 +1000

    Model String changed to string

--- Zorro.WebApplication/Models/Transaction.cs
+++ Zorro.WebApplication/Models/Transaction.cs
@@ -12,20 +12,15 @@ namespace Zorro.WebApplication.Models
         public TransactionType TransactionType { get; set; }
 
         [ForeignKey("DestinationAccount")]
-        [RegularExpression(@"^(\d{4})$", ErrorMessage = "Error: Must be 4 Digits.")]
         public int? DestinationAccountNumber { get; set; }
         public virtual Account DestinationAccount { get; set; }
 
-        /* [Required, ForeignKey("Account"), Display(Name = "Account Number")]
-         [RegularExpression(@"^(\d{4})$", ErrorMessage = "Error: Must be 4 Digits.")]
-         public int AccountNumber { get; set; }
-         public virtual Account Account { get; set; }*/
         public decimal Amount { get; set; }
         public DateTime TransactionTimeUTC { get; set; }
 
         public CurrencyType CurrencyType { get; set; }
 
-        public String Comment { get; set; }
+        public string Comment { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
 
     }
@@ -54,68 +49,3 @@ namespace Zorro.WebApplication.Models
     }
 
 }
-
-/* old transaction model
- using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace VirtualWalletApp.Models
-{
-    //transaction model 
-
-    /* -- Currency Types --
-    US dollars 
-    Euro 
-    Japanese yen 
-    British pound 
-    Canadian dollar 
-    Swiss Franc 
-    
-public enum CurrencyType
-{
-    USD = 1,
-    Euro = 2,
-    Yen = 3,
-    Pound = 4,
-    CAD = 5,
-    Franc = 6
-}
-
-public enum TransactionType
-{
-    Deposit = 1,
-    Withdraw = 2,
-    Transfer = 3,
-    Commission = 4
-}
-public class Transaction
-{
-    [Required]
-    public int TransactionID { get; set; }
-
-    [Required, RegularExpression(@"^[1-4]$", ErrorMessage = "Error: Invalid Transaction Type")]
-    public TransactionType TransactionType { get; set; }
-
-    [Required, ForeignKey("Account"), Display(Name = "Account Number")]
-    [RegularExpression(@"^(\d{4})$", ErrorMessage = "Error: Must be 4 Digits.")]
-    public int AccountNumber { get; set; }
-    public virtual Account Account { get; set; }
-
-    [ForeignKey("DestinationAccount")]
-    [RegularExpression(@"^(\d{4})$", ErrorMessage = "Error: Must be 4 Digits.")]
-    public int? DestinationAccountNumber { get; set; }
-    public virtual Account DestinationAccount { get; set; }
-
-    [Required, Range(0, float.MaxValue, ErrorMessage = "Error: Please enter a number that is positive.")]
-    [DataType(DataType.Currency)]
-    [Column(TypeName = "decimal(18,2)")]
-    public decimal Amount { get; set; }
-
-    [StringLength(30, ErrorMessage = "Error: Comment has a maximum of 30 Characters.")]
-    public string Comment { get; set; }
-
-    [Required]
-    public DateTime TransactionTimeUtc { get; set; }
-}
-}
-*/

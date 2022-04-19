02b3adb022c70a4bc97ead25f14b1213e4aed42f
Luis D'abruzzo <s3630513@student.rmit.edu.au>
2022-04-19 18:33:41 +1000

    Started working on interface to generate and revoke API keys for eCommerce vendors

---
+++ Zorro.WebApplication/Data/IEcommerceVendorService.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography;
+
+namespace Zorro.WebApplication.Data
+{
+    public interface IEcommerceVendorService
+    {
+        string GenerateApiKey(ApplicationUser user);
+        string RevokeApiKey(ApplicationUser user, string apikey);
+    }
+
+    
+}

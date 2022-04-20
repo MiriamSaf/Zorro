6d2fec5b52b4436965afe894d0048d7086e1a188
Luis D'abruzzo <s3630513@student.rmit.edu.au>
2022-04-20 13:27:40 +1000

    Refactored transfer and bpay wallet and biller validation. Removed buttons.

--- Zorro.WebApplication/wwwroot/js/site.js
+++ Zorro.WebApplication/wwwroot/js/site.js
@@ -4,40 +4,56 @@
 // Write your JavaScript code.
 const validationClasses = ['is-invalid', 'is-valid']
 
-function verify(inputId, route, buttonId) {
-    try {
-        let uri = route + '?' + new URLSearchParams({
-            id: document.getElementById(inputId).value,
-        })
-        fetch(uri).then(function (response) {
-            let status = 0
-            if (response.ok) {
-                status = 1;
-            }
-            showVerificationStatus(status, inputId);
-            enableOrDisableButton(status, buttonId);
-        })
+async function verifyWallet(inputId, buttonId = null) {
+    let walletId = document.getElementById(inputId).value;
+    var result = await verifyWalletWithServer(walletId);
+    var status = result ? 1 : 0;
+    showVerificationStatus(status, inputId)
+    if (buttonId == null)
+        return
+    enableOrDisableButton(status, buttonId);
+}
 
-    } catch (e) {
-        showVerificationStatus(0, inputId);
-        console.error('Unable to verify', error)
+async function verifyWalletWithServer(walletId) {
+    // build uri with query string
+    let route = "VerifyWalletId";
+    let uri = route + '?' + new URLSearchParams({
+        id: walletId,
+    });
+
+    // make request to server to verify wallet ID
+    try {
+        let result = await fetch(uri);
+        if (result.ok)
+            return true;
+        else
+            return false;
+    } catch {
+        console.error("An error occurred during wallet validation");
+        return false;
     }
 }
 
-async function verifyBillerCode(inputId, buttonId) {
+async function verifyBillerCode(inputId, billerNameInputId, buttonId = null) {
     let status = 1;
     let billerInput = document.getElementById(inputId);
+    let billerNameInput = document.getElementById(billerNameInputId)
     let billerCode = billerInput.value;
 
+    let billerName = ""
     try {
-        let billerName = await getBpayBillerName(billerCode);
-        document.getElementById('billerName').value = billerName;
+        billerName = await getBpayBillerName(billerCode);
     } catch (e) {
+        console.error("An error when verifying biller code");
         status = 0;
     }
-    
+    billerNameInput.value = billerName;
+
     showVerificationStatus(status, inputId);
-    //enableOrDisableButton(status, buttonId)
+    if (buttonId == null) {
+        return;
+    }
+    enableOrDisableButton(status, buttonId);
 }
 
 async function getBpayBillerName(billerCode) {

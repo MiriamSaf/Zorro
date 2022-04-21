0a4138cd495781b62e48f1e31b6ee6058bcb5557
MiriamSaf <s3739725@student.rmit.edu.au>
2022-04-21 12:41:52 -0400

    Add capability to delete user from admin portal

--- Zorro.WebApplication/Controllers/UserController.cs
+++ Zorro.WebApplication/Controllers/UserController.cs
@@ -9,9 +9,12 @@ namespace Zorro.WebApplication.Models
     public class UserController : Controller
     {
         private UserManager<ApplicationUser> _userManager;
-        public UserController(UserManager<ApplicationUser> userManager)
+
+        private IPasswordHasher<ApplicationUser> _passwordHashed;
+        public UserController(UserManager<ApplicationUser> userManager, IPasswordHasher<ApplicationUser> passwordHashed)
         {
             _userManager = userManager;
+            _passwordHashed = passwordHashed;
         }
         public IActionResult Index()
         {
@@ -22,6 +25,7 @@ namespace Zorro.WebApplication.Models
 
         public ViewResult Create() => View();
 
+        //create new user 
         [HttpPost]
         public async Task<IActionResult> Create(ApplicationUser user)
         {
@@ -50,6 +54,70 @@ namespace Zorro.WebApplication.Models
                 }
             }
             return View(user);
+        }
+
+        public async Task<IActionResult> Update(string id)
+        {
+            ApplicationUser user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+                return View(user);
+            else
+                return RedirectToAction("Index");
+        }
+
+        //update user 
+        [HttpPost]
+        public async Task<IActionResult> Update(string id, string email, string password)
+        {
+            ApplicationUser user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                if (!string.IsNullOrEmpty(email))
+                    user.Email = email;
+                else
+                    ModelState.AddModelError("", "Email cannot be empty");
+
+                if (!string.IsNullOrEmpty(password))
+                    user.PasswordHash = _passwordHashed.HashPassword(user, password);
+                else
+                    ModelState.AddModelError("", "Password cannot be empty");
+
+                if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+                {
+                    IdentityResult result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Index");
+                    else
+                        Errors(result);
+                }
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View(user);
+        }
+
+        //delete 
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            ApplicationUser user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await _userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    Errors(result);
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View("Index", _userManager.Users);
+        }
+
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
         }
     }
 }

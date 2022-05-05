b4c9f935a3c6c9a78e9a797f512ab59a522f6bcf
Luis D'abruzzo <s3630513@student.rmit.edu.au>
2022-05-05 18:59:07 +1000

    Added authorization requirement to role creation screen

--- Zorro.WebApplication/Controllers/RoleController.cs
+++ Zorro.WebApplication/Controllers/RoleController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Zorro.WebApplication.ViewModels;
 
 namespace Zorro.WebApplication.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class RoleController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;

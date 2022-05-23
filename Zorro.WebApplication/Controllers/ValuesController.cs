e04ac97209c407c80f0f245dd0df9aca3d82e2bd
ClaireBanville <s3754278@student.rmit.edu.au>
2022-05-23 21:03:22 +1000

    Changed chat styling to a drop down box rather than a list - still need message to show recipent

---
+++ Zorro.WebApplication/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Zorro.WebApplication.Data;
+using Zorro.Dal.Models;
+using Zorro.WebApplication.Dtos;
+using Zorro.WebApplication.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Zorro.Dal;
+
+namespace Zorro.WebApplication.Controllers
+{
+    public class FriendingController : ControllerBase
+    {
+        public ActionResult Friending()
+        {
+            return RedirectToAction("Friending");
+        }
+    }
+}

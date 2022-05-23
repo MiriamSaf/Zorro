f1ac26c6555fd9d8cf1b933888b13ecbc89994c1
ClaireBanville <s3754278@student.rmit.edu.au>
2022-05-23 19:11:32 +1000

    Start of Friending Front end CS. and CSHTML file started

---
+++ Zorro.WebApplication/Views/Friending/Friends.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Zorro.WebApplication.Views.NewFolder
+{
+    public class FriendsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

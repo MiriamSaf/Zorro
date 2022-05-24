17c1e7e4a2608601f0d206ebd0acd5b14aa3e54d
Miriam Saftlas <S3739725@student.rmit.edu.au>
2022-05-23 21:02:24 -0400

    add banker and chathub comments

--- Zorro.WebApplication/Chat/ChatHub.cs
+++ Zorro.WebApplication/Chat/ChatHub.cs
@@ -5,17 +5,20 @@ using Zorro.Dal.Models;
 
 namespace Zorro.WebApplication.Chat
 {
+    //deals with functionality for chat feature 
     public class ChatHub : Hub
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<ApplicationUser> _userManager;
 
+        //DI for chat hub
         public ChatHub(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
             _userManager = userManager;
         }
 
+        //sends a message from a sender to a recipient 
         public async Task SendMessage(string sender, string recipient, string message)
         {
             if (string.IsNullOrWhiteSpace(sender) || string.IsNullOrWhiteSpace(recipient) || string.IsNullOrWhiteSpace(message))
@@ -25,12 +28,14 @@ namespace Zorro.WebApplication.Chat
             await Clients.User(recipient).SendAsync("ReceiveMessage", sender, message);
             // send message back to sender so they see it in their chat window as well
             await Clients.Caller.SendAsync("ReceiveMessage", sender, message);
-
+            //saves the chat message
             await SaveChatMessage(sender, recipient, message, DateTime.Now);
         }
 
+        //saves the chat message with a timestamp
         private async Task SaveChatMessage(string sender, string recipient, string message, DateTime timestamp)
         {
+            //check for invalid message and that sender and recipient are both valid 
             if (string.IsNullOrWhiteSpace(message))
                 return;
             var senderUser = await _userManager.FindByEmailAsync(sender);
@@ -39,7 +44,7 @@ namespace Zorro.WebApplication.Chat
             var recipientuser = await _userManager.FindByEmailAsync(recipient);
             if (recipientuser is null)
                 return;
-
+            //save chat details 
             var chatMessage = new ChatMessage()
             {
                 Sender = senderUser,
@@ -47,11 +52,13 @@ namespace Zorro.WebApplication.Chat
                 Timestamp = timestamp,
                 Message = message
             };
+            //add the chat message to context and save 
             await _context.ChatMessages.AddAsync(chatMessage);
             await _context.SaveChangesAsync();
         }
     }
 
+    //get the user id of user 
     public class NameUserIdProvider : IUserIdProvider
     {
         public string GetUserId(HubConnectionContext connection)
--- Zorro.WebApplication/Data/Banker.cs
+++ Zorro.WebApplication/Data/Banker.cs
@@ -4,8 +4,10 @@ using Zorro.Dal;
 
 namespace Zorro.WebApplication.Data
 {
+    //banker class has all essential banker services 
     public interface IBanker
     {
+        //lits of all the different tasks of the banker
         Task<bool> BpayTransfer(Wallet sourceWallet, decimal amount, int BpayBillerCode, string comment, Currency currency = Currency.Aud, TransactionType transaction = TransactionType.BPay);
         Task DepositFunds(Wallet destinationWallet, decimal amount, string comment, Currency currency = Currency.Aud,
           TransactionType transactionType = TransactionType.Payment);
@@ -22,28 +24,33 @@ namespace Zorro.WebApplication.Data
         private readonly ILogger<Banker> _logger;
         private readonly ApplicationDbContext _applicationDbContext;
 
+        //DI for the banker class
         public Banker(ILogger<Banker> logger, ApplicationDbContext applicationDbContext)
         {
             _logger = logger;
             _applicationDbContext = applicationDbContext;
         }
 
+        //gets a wallet by passing in the users display name
         public async Task<Wallet> GetWalletByDisplayName(string displayName)
         {
+            //finds the users wallet based on their name 
             string normalizedDisplayName = displayName.ToUpper();
             var result = _applicationDbContext.Wallets
                 .Include(x => x.ApplicationUser)
                 .Where(y => y.ApplicationUser.NormalizedEmail == normalizedDisplayName);
-
+            //returns users wallet
             return await result.FirstOrDefaultAsync();
         }
 
+        //allows for funds to be transfered from a source to a destination 
         public async Task TransferFunds(Wallet sourceWallet, Wallet destinationWallet,
             decimal amount, string comment, Currency currency = Currency.Aud,
             TransactionType transactionType = TransactionType.Transfer)
         {
 
             var now = DateTime.Now;
+            //create the source transaction
             var sourceTransaction = new Transaction()
             {
                 Amount = amount * -1,
@@ -53,7 +60,7 @@ namespace Zorro.WebApplication.Data
                 TransactionType = TransactionType.Transfer,
                 Wallet = sourceWallet
             };
-
+            //create the destination that the money will go to as a transaction
             var destinationTransaction = new Transaction()
             {
                 Amount = amount,
@@ -63,22 +70,24 @@ namespace Zorro.WebApplication.Data
                 TransactionType = TransactionType.Transfer,
                 Wallet = destinationWallet
             };
-
+            //add the source transaction
             await _applicationDbContext.AddAsync(sourceTransaction);
             sourceWallet.Balance += sourceTransaction.Amount;
             _applicationDbContext.Wallets.Update(sourceWallet);
-
+            //add the destination transaction
             await _applicationDbContext.AddAsync(destinationTransaction);
             destinationWallet.Balance += destinationTransaction.Amount;
             _applicationDbContext.Wallets.Update(sourceWallet);
-
+            //save the changes
             await _applicationDbContext.SaveChangesAsync();
         }
 
+        //deposit funds function
         public async Task DepositFunds(Wallet destinationWallet, decimal amount, string comment, Currency currency = Currency.Aud,
           TransactionType transactionType = TransactionType.Payment)
         {
             var now = DateTime.Now;
+            //add a deposit transaction
             var depositTransaction = new Transaction()
             {
                 Amount = amount,
@@ -90,7 +99,7 @@ namespace Zorro.WebApplication.Data
             };
 
            
-
+            //add additional checks 
             if (amount > 0)
             {
                 destinationWallet.Balance += amount;
@@ -98,11 +107,12 @@ namespace Zorro.WebApplication.Data
                 await _applicationDbContext.AddAsync(depositTransaction);
 
             }
+            //save the changes 
             await _applicationDbContext.SaveChangesAsync();
         }
 
 
-
+        //get transactions by passing in the wallet id 
         public async Task<List<Transaction>> GetTransactionsByWallet(Guid walletId)
         {
             var results = await _applicationDbContext
@@ -112,15 +122,18 @@ namespace Zorro.WebApplication.Data
             return results;
         }
 
+        //verify the users balance by passing in their walletid and an amount
         public async Task<bool> VerifyBalance(Guid walletId, decimal amount)
         {
             var result = await _applicationDbContext.Wallets.FindAsync(walletId);
             return result.Balance >= amount;
         }
 
+        //bpay transfer - to make a bpay
         public async Task<bool> BpayTransfer(Wallet sourceWallet, decimal amount, int BpayBillerCode, string comment, Currency currency = Currency.Aud, TransactionType transaction = TransactionType.BPay)
         {
             var now = DateTime.Now;
+            //create a bpay transaction that will be entered into system
             var bpayTransaction = new Transaction()
             {
                 Amount = amount * -1,
@@ -137,7 +150,7 @@ namespace Zorro.WebApplication.Data
             {
                 return false;
             }
-
+            //amoutn above zero
             if (amount > 0)
             {
                 //minus amount from balance
@@ -154,9 +167,11 @@ namespace Zorro.WebApplication.Data
             return false;
         }
 
+        //make a shop purchase 
         public async Task ShopPurchase(Wallet sourceWallet, decimal amount)
         {
             var now = DateTime.Now;
+            //create a shop transaction
             var shopTransaction = new Transaction()
             {
                 Amount = amount,
@@ -165,7 +180,7 @@ namespace Zorro.WebApplication.Data
                 TransactionType = TransactionType.Shop,
                 Wallet = sourceWallet
             };
-
+            //add the transaction into the system
             await _applicationDbContext.AddAsync(shopTransaction);
 
             await _applicationDbContext.SaveChangesAsync();

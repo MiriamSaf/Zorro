8c2a42d1a6efe719c8a9235a658c6fec0b382245
Damien MacDown <S3493516@student.rmit.edu.au>
2022-05-25 19:57:38 +1000

    DBUpdate

---
+++ Zorro.Dal/Migrations/20220525094413_DBUpdate2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zorro.Dal.Migrations
+{
+    public partial class DBUpdate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LockoutEnd",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "LockoutEnd",
+                table: "AspNetUsers",
+                type: "datetimeoffset",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
